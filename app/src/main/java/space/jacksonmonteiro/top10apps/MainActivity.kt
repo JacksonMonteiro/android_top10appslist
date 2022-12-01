3692cb98aa7c70f78f58f652e0b99dc58085b8a1
jacksonmonteiro <infor.jackson324@gmail.com>
2022-12-01 10:09:16 -0300

    Some features added to menu and app

--- app/src/main/java/space/jacksonmonteiro/top10apps/MainActivity.kt
+++ app/src/main/java/space/jacksonmonteiro/top10apps/MainActivity.kt
@@ -3,6 +3,7 @@ package space.jacksonmonteiro.top10apps
 import android.content.Context
 import android.os.AsyncTask
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
@@ -31,42 +32,78 @@ class FeedEntry {
 class MainActivity : AppCompatActivity() {
     private var downloadData: DownloadData? = null
 
+    private var feedUrl: String = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=%d/xml"
+    private var feedLimit = 10
+
+    private var feedCacheURL = "INVALIDATED"
+    private val STATE_URL = "feedUrl"
+    private val STATE_LIMIT = "feedLimit"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        downloadUrl("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml")
+
+        if (savedInstanceState != null) {
+            feedUrl = savedInstanceState.getString(STATE_URL)!!;
+            feedLimit = savedInstanceState.getInt(STATE_LIMIT);
+        }
+
+        downloadUrl(feedUrl.format(feedLimit))
     }
 
     private fun downloadUrl(feedUrl: String) {
-        downloadData = DownloadData(this, findViewById(R.id.xmlListView))
-        downloadData?.execute(feedUrl)
+        if (feedUrl != feedCacheURL) {
+            downloadData = DownloadData(this, findViewById(R.id.xmlListView))
+            downloadData?.execute(feedUrl)
+            feedCacheURL = feedUrl
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.feeds_menu, menu)
+
+        if (feedLimit == 10) {
+            menu?.findItem(R.id.menu10)?.isChecked = true
+        } else {
+            menu?.findItem(R.id.menu25)?.isChecked = true
+        }
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        var feedUrl =
-            "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml"
-
         when (item.itemId) {
             R.id.menuFree -> feedUrl =
-                "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml"
+                "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=%d/xml"
             R.id.menuPaid -> feedUrl =
-                "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=10/xml"
+                "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=%d/xml"
             R.id.menuSongs -> feedUrl =
-                "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=10/xml"
+                "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=%d/xml"
+            R.id.menu10, R.id.menu25 -> {
+                if (!item.isChecked) {
+                    item.isChecked = true
+                    feedLimit = 35 - feedLimit
+                    Log.d("onOptionsItemSelected", "${item.title} settings feedLimit to $feedLimit")
+                } else {
+                    Log.d("onOptionsItemSelected", "feedLimit unchanged")
+                }
+            }
+            R.id.menuRefresh -> feedCacheURL = "INVALIDATED"
+            else -> return super.onOptionsItemSelected(item)
         }
 
-        downloadUrl(feedUrl)
+        downloadUrl(feedUrl.format(feedLimit))
         return true
     }
 
     override fun onDestroy() {
         super.onDestroy()
         downloadData?.cancel(true)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(STATE_URL, feedUrl)
+        outState.putInt(STATE_LIMIT, feedLimit)
     }
 
     companion object {
